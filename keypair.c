5aef157e76ea2bd73c165ea407a29913136a752d
maybe91 <99maybe99@mail.ru>
2025-01-13 12:24:10 +0000

    Refactor keypair generation to use EVP for key management and improve error handling

--- keypair.c
+++ keypair.c
@@ -4,16 +4,15 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/provider.h>
+#include <openssl/evp.h>
 #include <oqs/oqs.h>
 
-#define SIGNATURE_ALGORITHM OQS_SIG_alg_dilithium_5
 int main(int argc, char **argv)
 {
+
     if (argc != 3)
     {
         fprintf(stderr, "usage: %s <public-key> <private-key>\n", argv[0]);
@@ -22,60 +21,75 @@ int main(int argc, char **argv)
     const char *public_key_path = argv[1];
     const char *secret_key_path = argv[2];
 
-    OQS_SIG *sig = OQS_SIG_new(SIGNATURE_ALGORITHM);
+    OSSL_PROVIDER *default_prov = OSSL_PROVIDER_load(NULL, "default");
+    if (default_prov == NULL)
 
-    if (sig == NULL)
     {
+        ERR_print_errors_fp(stderr);
+        return EXIT_FAILURE;
+    }
+    OSSL_PROVIDER *oqs_prov = OSSL_PROVIDER_load(NULL, "oqsprovider");
+    if (oqs_prov == NULL)
+    {
+        ERR_print_errors_fp(stderr);
         return EXIT_FAILURE;
     }
 
-    // tworzenie kluczy
-    uint8_t *public_key = malloc(sig->length_public_key);
-    uint8_t *secret_key = malloc(sig->length_secret_key);
+    EVP_PKEY *pkey = NULL;
+    EVP_PKEY_CTX *pctx = NULL;
+    FILE *private_key_file = NULL;
+    FILE *public_key_file = NULL;
 
-    if (OQS_SIG_keypair(sig, public_key, secret_key) != OQS_SUCCESS) // Sprawdzenie tego, czy kluczy zostały stworzone
+    pctx = EVP_PKEY_CTX_new_from_name(NULL, "Dilithium5", NULL);
+    if (pctx == NULL)
     {
-        OQS_SIG_free(sig);
-        free(public_key);
-        free(secret_key);
-        return false;
-    }
-    printf("Kluczy zostały stworzone");
-
-    // Zapisywanie klucza publicznego
-    FILE *public_key_file = fopen(public_key_path, "wb");
-    if (public_key_file == NULL)
-    {
-        perror("Failed to open public key file");
-        OQS_SIG_free(sig);
-        free(public_key);
-        free(secret_key);
-        return EXIT_FAILURE;
+        fprintf(stderr, "Błąd podczas tworzenia kontekstu EVP dla Dilithium5\n");
+        return 1;
     }
 
-    fwrite(public_key, 1, sig->length_public_key, public_key_file);
-    fclose(public_key_file);
+    if (EVP_PKEY_keygen_init(pctx) <= 0)
+    {
+        fprintf(stderr, "Błąd inicjalizacji generowania klucza\n");
+        return 1;
+    }
 
-    // Zapisywanie klucza prywatnego
-    FILE *private_key_file = fopen(secret_key_path, "wb");
+    if (EVP_PKEY_keygen(pctx, &pkey) <= 0)
+    {
+        fprintf(stderr, "Błąd podczas generowania klucza\n");
+        return 1;
+    }
+
+    private_key_file = fopen(secret_key_path, "wb");
     if (private_key_file == NULL)
     {
-        perror("Failed to open private key file");
-        OQS_SIG_free(sig);
-        free(public_key);
-        free(secret_key);
-        return EXIT_FAILURE;
+        fprintf(stderr, "Nie można otworzyć pliku na zapis klucza prywatnego\n");
+        return 1;
+    }
+    if (PEM_write_PrivateKey(private_key_file, pkey, EVP_aes_256_cbc(), NULL, 0, NULL, NULL) == 0)
+    {
+        fprintf(stderr, "Błąd zapisu klucza prywatnego\n");
+        return 1;
     }
 
-    fwrite(secret_key, 1, sig->length_secret_key, private_key_file);
+    public_key_file = fopen(public_key_path, "wb");
+    if (public_key_file == NULL)
+    {
+        fprintf(stderr, "Nie można otworzyć pliku na zapis klucza publicznego\n");
+        return 1;
+    }
+    if (PEM_write_PUBKEY(public_key_file, pkey) == 0)
+    {
+        fprintf(stderr, "Błąd zapisu klucza publicznego\n");
+        return 1;
+    }
+
     fclose(private_key_file);
+    fclose(public_key_file);
+    EVP_PKEY_free(pkey);
+    EVP_PKEY_CTX_free(pctx);
+    OSSL_PROVIDER_unload(default_prov);
+    OSSL_PROVIDER_unload(oqs_prov);
 
-    printf("Keys saved successfully\n");
-
-    // Zwolnienie zasobów
-    OQS_SIG_free(sig);
-    free(public_key);
-    free(secret_key);
-
-    return EXIT_SUCCESS;
+    printf("Klucze zostały zapisane do plików: private_key.pem i public_key.pem\n");
+    return 0;
 }
